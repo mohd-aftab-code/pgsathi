f9e4f5f202fe82a07287d7f93af4b8456a7f0a8f
mohd-aftab-code <aftabansari92751@gmail.com>
2026-06-24 16:58:48 +0530

    feat: add i.pravatar.cc and images.unsplash.com to image remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
     formats: ["image/avif", "image/webp"],
   },
